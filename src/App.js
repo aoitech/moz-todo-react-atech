d0d48222dc2aaa899b39dc4d5d99fb142f568d75
aoitech <aoi19900722@yahoo.co.jp>
2023-01-28 11:19:46 +0900

    changed subject

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App(props) {
-  const subject = "React";
+  const subject = "React!";
   return (
     <div className="App">
       <header className="App-header">
